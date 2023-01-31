d9914d351c712150cfd1acdab6c3cf7769bb1e32
Serena-Dong <serenadong3159@icloud.com>
2023-01-31 18:32:31 +0100

    contents

---
+++ src/components/contents/button.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'button'
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/contents/partnerships.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'partnerships'
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
