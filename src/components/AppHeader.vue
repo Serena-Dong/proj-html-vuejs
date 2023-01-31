48fb155c2c76bf2fa1d27990b7ec8f13ae99c437
Serena-Dong <serenadong3159@icloud.com>
2023-01-31 18:25:56 +0100

    AppHeader structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,30 @@
 <script>
-
+export default {
+    name: 'AppHeader'
+}
 </script>
 
 <template>
+    <header>
+        <div class="container">
 
+            <!-- LOGO -->
+            <div class="logo">
+            </div>
+            <!-- NAV -->
+            <div class="nav"></div>
+
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.container {
+    height: 100px;
+    border: 2px solid black;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
