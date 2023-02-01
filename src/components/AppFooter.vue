c4688e99885ecf4c3e1c4530995ad54a48591e17
Serena-Dong <serenadong3159@icloud.com>
2023-02-01 12:07:38 +0100

    AppFooter blocks structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,9 +5,26 @@ export default {
 </script>
 
 <template>
-
+    <div class="container">
+        <div class="footer-top"></div>
+        <div class="footer-bottom"></div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.container {
+    height: 600px;
+    border: 2px solid black;
 
+    .footer-top {
+        height: 400px;
+        border: 2px solid black;
+
+    }
+
+    .footer-bottom {
+        height: 200px;
+        border: 2px solid black;
+    }
+}
 </style>
