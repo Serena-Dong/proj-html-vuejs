226186dd2642ef5a0dc5d968230d9dc890806f23
Serena-Dong <serenadong3159@icloud.com>
2023-02-02 12:14:23 +0100

    button changes

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,13 +2,12 @@
 import jumbotron from './contents/jumbotron.vue'
 import section from './contents/section.vue'
 import partnerships from './contents/partnerships.vue'
-import button from './contents/button.vue'
 
 
 export default {
     name: 'AppMain',
     components: {
-        jumbotron, section, partnerships, button
+        jumbotron, section, partnerships
     }
 
 }
