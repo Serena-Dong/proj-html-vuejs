c699b86839cf2c8d7ceabb0275e16052c70bae34
Serena-Dong <serenadong3159@icloud.com>
2023-02-02 22:37:39 +0100

    little changes

--- src/data/data.js
+++ src/data/data.js
@@ -56,7 +56,7 @@ const mainData = {
         ],
     intern: {
         img: 'http://localhost:5173/src/assets/img/testimonials/large-testimonial.jpg',
-        text: 'The team at Avada Marketing Consultant is fabulous. They helped us unlock our potential online and offline. We have experienced year on year growth due to their progressive approch',
+        text: 'The team at Avada Marketing Consultant is fabulous. They helped us unlock our potential online and offline. We have experienced year on year growth due to their progressive approch.',
         name: 'Kate Schadler',
         role: 'Marketing Consultant - Abstract'
     },
