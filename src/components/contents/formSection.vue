2693fc255af6ae9967656ca0849a2a74adb6b5ba
Serena-Dong <serenadong3159@icloud.com>
2023-02-02 13:03:22 +0100

    form profile

--- src/components/contents/formSection.vue
+++ src/components/contents/formSection.vue
@@ -1,16 +1,26 @@
 <script>
+import { generalData } from '../../data/data'
 export default {
-    name: 'formSection'
+    name: 'formSection',
+    data() {
+        return {
+            generalData
+        }
+    }
 }
 </script>
 
 <template>
     <div class="form-box">
         <div class="profile">
-            <div class="profile-pic">
-                <img src="" alt="">
+            <div id="profile-pic">
+                <img src="../../assets/img/marketing-consultant-expert.jpg" alt="">
             </div>
+            <p id="name">Richard Madsen</p>
+            <p id="role">Marketing Consultat Expert</p>
+            <p id="phone-number"><i class="fa-solid fa-phone"></i>{{ generalData.phoneNumber }}</p>
         </div>
+        .
     </div>
 </template>
 
@@ -25,5 +35,31 @@ export default {
     background-color: $white;
     border-radius: 10px;
     box-shadow: 5px 5px 100px $grey;
+}
+
+.profile {
+    text-align: center;
+
+    #profile-pic img {
+        height: 70px;
+        border-radius: 5rem;
+    }
+
+    #name {
+        font-size: 1.5rem;
+    }
+
+    #role {
+        color: $grey;
+    }
+
+    #phone-number {
+        color: $ochre;
+        margin-right: 1rem;
+    }
+}
+
+.profile>* {
+    margin: 1rem;
 }
 </style>
