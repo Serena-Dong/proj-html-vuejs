b2dab69de194106a2bb9cde76ea89d1c3352c66f
Serena-Dong <serenadong3159@icloud.com>
2023-02-02 15:19:20 +0100

    card

---
+++ src/components/contents/card.vue
@@ -0,0 +1,103 @@
+<script>
+import btn from './button.vue'
+
+export default {
+    name: 'card',
+    components: { btn },
+    data() {
+        return {
+            text: 'Get a Consultant'
+        }
+    }
+
+}
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="img-col">
+                    <img src="../../assets/img/card/img1.svg" alt="">
+                </div>
+                <div class="text-col">
+                    <h4>Business Growth</h4>
+                    <h1>Increase Brand Awareness</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt asperiores ut porro in eligendi
+                        reiciendis, facilis repudiandae error voluptatem sequi ad!</p>
+                    <btn :text="text"></btn>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="text-col">
+                    <h4>Investors in People</h4>
+                    <h1>In-House Sales Training</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo nemo hic nihil praesentium!
+                        Aliquid culpa consectetur esse cum molestiae fugiat reprehenderit atque dolores.</p>
+                    <btn :text="text"></btn>
+                </div>
+                <div class="img-col">
+                    <img src="../../assets/img/card/img2.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="img-col">
+                    <img src="../../assets/img/card/img3.svg" alt="">
+                </div>
+                <div class="text-col">
+                    <h4>Social Media Analysis</h4>
+                    <h1>Harness Your Social Proof</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi dolores voluptate quas quaerat
+                        ipsum adipisci neque ipsam beatae impedit nihil obcaecati iusto, qui quasi.</p>
+                    <btn :text="text"></btn>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.container {
+    height: 500px;
+
+    margin-top: 5rem;
+    margin-bottom: 5rem;
+
+    .row {
+        display: flex;
+        justify-content: center;
+        gap: 3%;
+        height: 500px;
+
+
+        .img-col {
+            width: 500px;
+        }
+
+        .text-col {
+
+            margin-top: 3rem;
+            display: block;
+            width: 400px;
+            font-size: 1.3rem;
+
+            p {
+                font-size: 1.2rem;
+                line-height: 2.2rem;
+            }
+        }
+
+        .text-col>* {
+            margin-bottom: 2rem;
+        }
+    }
+}
+</style>
--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,26 @@ const headerData = {
     navbar: ['Home', 'Services', 'Why Us', 'Case Studies', 'About', 'Blog']
 }
 
+const mainData = {
+    card: {
+        img: [
+            '../../assets/img/card/img1.svg',
+            '../../assets/img/card/img2.svg',
+            '../../assets/img/card/img1.svg'
+        ],
+        title: [
+            'Increase Brand Awareness',
+            'In-House Sales Training',
+            'Harness Your Social Proof'
+        ],
+        description: [
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio velit libero id, illo iste deserunt nesciunt.',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis asperiores excepturi obcaecati qui dignissimos?',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti, dolorum assumenda omnis ab nobis aliquam!'
+        ]
+    }
+}
+
 const footerData = {
 
     services: ['Marketing Plan', 'Sales Development', 'Digital Marketing', 'Pricing', 'Why Us', 'Case Studios'],
@@ -14,5 +34,5 @@ const footerData = {
 const generalData = {
     phoneNumber: '(555) 802-1234'
 }
-export { headerData, footerData, generalData }
+export { headerData, mainData, footerData, generalData }
 
