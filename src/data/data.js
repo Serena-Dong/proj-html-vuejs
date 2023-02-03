ebb861ea2c348ef7ccac026cb3ef5b094a301518
Serena-Dong <serenadong3159@icloud.com>
2023-02-03 12:04:56 +0100

    little changes

--- src/data/data.js
+++ src/data/data.js
@@ -122,7 +122,7 @@ const footerData = {
     services: ['Marketing Plan', 'Sales Development', 'Digital Marketing', 'Pricing', 'Why Us', 'Case Studios'],
     resources: ['Learning Center', 'Video Tutorials', 'Customers', 'Blog'],
     company: ['Who We Are', 'Contact Us', 'Carrers'],
-    credits: ['2020 Avada Consultant Powered by WordPress', 'Call Us (555) 802 1234', 'info@yourcompany.com']
+    credits: ['© 2020 • Avada Consultant • Powered by WordPress', 'Call Us (555) 802 1234', 'info@yourcompany.com']
 }
 
 const generalData = {
